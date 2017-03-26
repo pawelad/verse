08dab594b958fe49e2a640cb675cf9ec8905d4b3
Paweł Adamczak <pawel.ad@gmail.com>
2017-03-26 12:47:25 +0100

    Added 'versions.views' tests

---
+++ tests/versions/test_views.py
@@ -0,0 +1,154 @@
+"""
+Test `versions.views` file
+"""
+from django.http import Http404
+from django.utils.crypto import get_random_string
+
+from rest_framework import viewsets, status
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
+import pytest
+
+from checkers.projects import PythonVersionChecker
+from versions.views import ProjectsVersionsViewSet
+
+
+available_projects = {'python': PythonVersionChecker}
+
+
+class TestProjectsVersionsViewSet:
+    """
+    Tests for 'versions.views.ProjectsVersionsViewSet'
+    """
+    client = APIClient()
+    view = ProjectsVersionsViewSet
+    base_name = 'versions'
+
+    def test_view_inheritance(self):
+        """Test view inheritance name"""
+        assert isinstance(self.view(), viewsets.ReadOnlyModelViewSet)
+
+    def test_view_lookup_field(self):
+        """Test view lookup field"""
+        assert self.view.lookup_field == 'name'
+
+    @pytest.mark.parametrize('suffix,expected', [
+        ('List', 'Projects list'),
+        ('Instance', 'Latest project version'),
+        ('Major', 'Latest major versions'),
+        ('Minor', 'Latest minor versions'),
+    ])
+    def test_view_get_view_name_method(self, suffix, expected):
+        """Test view `get_view_name()` method"""
+        instance = self.view()
+        instance.suffix = suffix
+
+        assert instance.get_view_name() == expected
+
+    def test_view_get_object_method(self, mocker):
+        """Test view `get_object()` method"""
+        mocker.patch('versions.views.AVAILABLE_CHECKERS', available_projects)
+        instance = self.view()
+
+        instance.kwargs = {'name': 'python'}
+        assert isinstance(instance.get_object(), PythonVersionChecker)
+
+        instance.kwargs = {'name': get_random_string()}
+        with pytest.raises(Http404):
+            instance.get_object()
+
+    def test_view_list_method(self, mocker):
+        """Test view `list()` method"""
+        mocker.patch('versions.utils.AVAILABLE_CHECKERS', available_projects)
+
+        url = reverse('{0.base_name}-list'.format(self))
+        response = self.client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'python': {
+                'homepage': 'https://www.python.org/',
+                'latest': 'http://testserver/api/v1/python/',
+                'latest_major': 'http://testserver/api/v1/python/major/',
+                'latest_minor': 'http://testserver/api/v1/python/minor/'
+            },
+        }
+
+    def test_view_retrieve_method(self, mocker):
+        """Test view `retrieve()` method"""
+        mocker.patch('versions.views.AVAILABLE_CHECKERS', available_projects)
+
+        latest_version = '0.1.1'
+        mocked_get_latest_version = mocker.patch(
+            'versions.views.get_latest_version',
+        )
+        mocked_get_latest_version.return_value = latest_version
+
+        url = reverse('{0.base_name}-detail'.format(self), args=['python'])
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'latest': latest_version,
+        }
+
+        url = reverse(
+            '{0.base_name}-detail'.format(self), args=[get_random_string()],
+        )
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+        assert mocked_get_latest_version.call_count == 1
+
+    def test_view_major_method(self, mocker):
+        """Test view `major()` method"""
+        mocker.patch('versions.views.AVAILABLE_CHECKERS', available_projects)
+
+        latest_versions = {
+            '1': '1.2.3',
+            '0': '0.12.0',
+        }
+        mocked_get_latest_major_versions = mocker.patch(
+            'versions.views.get_latest_major_versions',
+        )
+        mocked_get_latest_major_versions.return_value = latest_versions
+
+        url = reverse('{0.base_name}-major'.format(self), args=['python'])
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == latest_versions
+
+        url = reverse(
+            '{0.base_name}-major'.format(self), args=[get_random_string()],
+        )
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+        assert mocked_get_latest_major_versions.call_count == 1
+
+    def test_view_minor_method(self, mocker):
+        """Test view `minor()` method"""
+        mocker.patch('versions.views.AVAILABLE_CHECKERS', available_projects)
+
+        latest_versions = {
+            '1.2': '1.2.3',
+            '1.1': '1.1.4',
+            '1.0': '1.0.2',
+        }
+        mocked_get_latest_minor_versions = mocker.patch(
+            'versions.views.get_latest_minor_versions',
+        )
+        mocked_get_latest_minor_versions.return_value = latest_versions
+
+        url = reverse('{0.base_name}-minor'.format(self), args=['python'])
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == latest_versions
+
+        url = reverse(
+            '{0.base_name}-minor'.format(self), args=[get_random_string()],
+        )
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+        assert mocked_get_latest_minor_versions.call_count == 1
