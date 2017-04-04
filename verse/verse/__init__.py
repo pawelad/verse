ba1be888b192386776901b1d9767f8f1adf1b5c5
Paweł Adamczak <pawel.ad@gmail.com>
2017-04-04 20:20:15 +0100

    Version bump

--- verse/verse/__init__.py
+++ verse/verse/__init__.py
@@ -7,7 +7,7 @@ from verse.celery import app as celery_app  # noqa
 
 
 __title__ = 'verse'
-__version__ = '0.1.0'
+__version__ = '1.0.1'
 __author__ = 'Paweł Adamczak'
-__license__ = 'MIT'
+__license__ = 'Apache 2'
 __url__ = 'https://github.com/pawelad/verse'
